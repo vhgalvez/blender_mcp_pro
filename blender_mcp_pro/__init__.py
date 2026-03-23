12c513ab1d901e936b9aecb6f0b4885060981c15
Victor Galvez <vhgalvez@gmail.com>
2026-03-23 12:37:49 +0100

    update

--- blender_mcp_pro/__init__.py
+++ blender_mcp_pro/__init__.py
@@ -1,3 +1,11 @@
-from .addon import bl_info, register, unregister
+bl_info = {
+    "name": "Blender MCP",
+    "author": "BlenderMCP",
+    "version": (1, 3),
+    "blender": (3, 0, 0),
+    "location": "View3D > Sidebar > Blender MCP",
+    "description": "Connect Blender to local MCP clients with authenticated commands.",
+    "category": "Interface",
+}
 
-__all__ = ["bl_info", "register", "unregister"]
+from .addon import register, unregister
